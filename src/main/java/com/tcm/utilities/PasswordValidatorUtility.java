f50e5cc8c93c4ef740e092550b24f7d4a9320551
Tonmoy Chowdhury <tonmoy.chowdhury@maersk.com>
2023-03-01 19:47:33 +0000

    added failure test scenarios for all rules

--- src/main/java/com/tcm/utilities/PasswordValidatorUtility.java
+++ src/main/java/com/tcm/utilities/PasswordValidatorUtility.java
@@ -29,34 +29,38 @@ public class PasswordValidatorUtility {
                 .filter(Objects::nonNull)
                 .filter(p -> p.length() > 8)
                 .flatMap(e -> Mono.just(true))
-                .switchIfEmpty(Mono.error(new IllegalArgumentException("Password Length invalid.")));
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("Password should be larger than 8 chars.")));
     }
 
     private Mono<Boolean> validateNullabilityRule(String password) {
         return Mono.just(password)
                 .filter(Objects::nonNull)
-                .hasElement();
+                .flatMap(e -> Mono.just(true))
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("Password should not be null.")));
     }
 
     private Mono<Boolean> validateUpperCaseRule(String password) {
         return Mono.just(password)
                 .filter(Objects::nonNull)
                 .filter(p -> p.chars().anyMatch(Character::isUpperCase))
-                .hasElement();
+                .flatMap(e -> Mono.just(true))
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("Password should have one uppercase letter at least.")));
     }
 
     private Mono<Boolean> validateLowerCaseRule(String password) {
         return Mono.just(password)
                 .filter(Objects::nonNull)
                 .filter(p -> p.chars().anyMatch(Character::isLowerCase))
-                .hasElement();
+                .flatMap(e -> Mono.just(true))
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("Password should have one lowercase letter at least.")));
     }
 
     private Mono<Boolean> validateDigitRule(String password) {
         return Mono.just(password)
                 .filter(Objects::nonNull)
                 .filter(p -> p.chars().anyMatch(Character::isDigit))
-                .hasElement();
+                .flatMap(e -> Mono.just(true))
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("Password should have one number at least.")));
     }
 
 }
--- src/test/java/com/tcm/utilities/PasswordValidatorUtilityTest.java
+++ src/test/java/com/tcm/utilities/PasswordValidatorUtilityTest.java
@@ -11,8 +11,44 @@ class PasswordValidatorUtilityTest {
     @Test
     void testWhenAllPasswordCriteriaSucceeds() {
 
-        StepVerifier.create(PasswordValidatorUtility.isPasswordValid("sfd"))
+        StepVerifier.create(PasswordValidatorUtility.isPasswordValid("dsfdDFF454sfsdsd"))
                 .expectNext(true)
                 .verifyComplete();
     }
+
+    @Test
+    void testWhenAllPasswordLengthCriteriaFails() {
+        StepVerifier.create(PasswordValidatorUtility.isPasswordValid("dffF5D"))
+                .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException && "Password should be larger than 8 chars.".equals(throwable.getMessage()))
+                .verify();
+    }
+
+    @Test
+    void testWhenAllPasswordNullabilityCriteriaFails() {
+        StepVerifier.create(PasswordValidatorUtility.isPasswordValid(null))
+                .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException && "Password should not be null.".equals(throwable.getMessage()))
+                .verify();
+    }
+
+    @Test
+    void testWhenAllPasswordUpperCaseCriteriaFails() {
+        StepVerifier.create(PasswordValidatorUtility.isPasswordValid("dffsf23fd"))
+                .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException && "Password should have one uppercase letter at least.".equals(throwable.getMessage()))
+                .verify();
+    }
+
+    @Test
+    void testWhenAllPasswordLowerCaseCriteriaFails() {
+        StepVerifier.create(PasswordValidatorUtility.isPasswordValid("DSDS343SDS"))
+                .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException && "Password should have one lowercase letter at least.".equals(throwable.getMessage()))
+                .verify();
+    }
+
+    @Test
+    void testWhenAllPasswordDigitCriteriaFails() {
+        StepVerifier.create(PasswordValidatorUtility.isPasswordValid("sdfDfdFDF"))
+                .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException && "Password should have one number at least.".equals(throwable.getMessage()))
+                .verify();
+    }
+
 }
