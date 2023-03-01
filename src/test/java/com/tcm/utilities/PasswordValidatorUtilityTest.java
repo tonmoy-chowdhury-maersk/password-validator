dcd72fac0fabcf7a3ee57b0b4b4389d55af80c46
Tonmoy Chowdhury <tonmoy.chowdhury@maersk.com>
2023-03-01 22:00:55 +0000

    added success test scenarios for minimum 3 valid rules criteria

--- src/test/java/com/tcm/utilities/PasswordValidatorUtilityTest.java
+++ src/test/java/com/tcm/utilities/PasswordValidatorUtilityTest.java
@@ -1,10 +1,7 @@
 package com.tcm.utilities;
 
 import org.junit.jupiter.api.Test;
-import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordValidatorUtilityTest {
 
@@ -49,6 +46,49 @@ class PasswordValidatorUtilityTest {
         StepVerifier.create(PasswordValidatorUtility.isPasswordValid("sdfDfdFDF"))
                 .expectErrorMatches(throwable -> throwable instanceof IllegalArgumentException && "Password should have one number at least.".equals(throwable.getMessage()))
                 .verify();
+    }
+
+    @Test
+    void testNullabilityUpperAndLowerCaseValid() {
+        // Valid due to successful Nullability, UpperCase and LowerCase criteria
+        StepVerifier.create(PasswordValidatorUtility.isPasswordValid("dsfdDF"))
+                .expectNext(true)
+                .verifyComplete();
+    }
+
+    @Test
+    void testNullabilityDigitsAndLowerCaseValid() {
+        StepVerifier.create(PasswordValidatorUtility.isPasswordValid("dsf36df"))
+                .expectNext(true)
+                .verifyComplete();
+    }
+
+    @Test
+    void testNullabilityDigitsAndUpperCaseValid() {
+        StepVerifier.create(PasswordValidatorUtility.isPasswordValid("DG7N"))
+                .expectNext(true)
+                .verifyComplete();
+    }
+
+    @Test
+    void testNullabilityLengthAndUpperCaseValid() {
+        StepVerifier.create(PasswordValidatorUtility.isPasswordValid("DDFDFTTTSSR"))
+                .expectNext(true)
+                .verifyComplete();
+    }
+
+    @Test
+    void testNullabilityLengthAndLowerCaseValid() {
+        StepVerifier.create(PasswordValidatorUtility.isPasswordValid("fgdfgfdgdggdfg"))
+                .expectNext(true)
+                .verifyComplete();
+    }
+
+    @Test
+    void testNullabilityLengthAndDigitsValid() {
+        StepVerifier.create(PasswordValidatorUtility.isPasswordValid("4535675624"))
+                .expectNext(true)
+                .verifyComplete();
     }
 
 }
